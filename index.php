363669ea8ab0a3202f28c741cf198aa5c0c98056
Victor <tagoro9@gmail.com>
2013-07-25 17:10:57 +0100

    add php dode

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             <title>Wooden - Creative Landing Page</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <meta name="description" content="Wooden - Creative Landing Page">
-                    <meta name="author" content="Shy Design">
+                    <?php echo '<meta name="author" content="Shy Design">'; ?>
                         
                         <!-- Le styles -->
                         <link href="css/bootstrap.min.css" rel="stylesheet">
