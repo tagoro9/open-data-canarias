0c2addf0d632d53b2f37d743e39d0f3eccb10847
Victor <tagoro9@gmail.com>
2013-07-26 18:46:07 +0100

    add new images

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <meta name="description" content="Augwesome Reality">
                     <?php echo '<meta name="author" content="Luis M. Pérez & Victor Mora">'; ?>
-
+                        <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
                         <link href="css/bootstrap.min.css" rel="stylesheet">
                             <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
                                 <link href="css/custom.css" rel="stylesheet">
@@ -33,11 +33,11 @@
                         
                         <div class="app-buttons">
                             <a class="border-btn pull-left">
-                                <p>Restauración</p>
+                                <p style="text-align:center;">Restauración</p>
                                 <img src="img/rest.png">
                             </a>
                             <a class="border-btn pull-right">
-                                <p>Alojamiento</p>
+                                <p style="text-align:center;">Alojamiento</p>
                                 <img src="img/aloj.png">
                             </a>
                         </div>
